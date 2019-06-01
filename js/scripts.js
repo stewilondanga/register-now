33a0276f6dbcbdd8e62c4cefdc1b87f68deac998
Stewart ilondanga <stilondanga@gmail.com>
2019-06-02 00:19:37 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -11,18 +11,18 @@ const form = document.getElementById('main-form');
 const form_main = document.getElementById('intro');
 const signInLink = document.querySelectorAll('.link-btn');
 const signUpLink = document.querySelectorAll('.su_link-btn');
-/*
-		showSignInform = (e) => {
-			const mainIntro = e.target.parentNode.parentNode;
-			if (form.style.display === 'none') {
-				form.style.display = 'block';
-				mainIntro.style.display = 'none';
-			} else  {
-				form.style.display = 'none';
-				mainIntro.style.display = 'block';
-			}
-		}
 
+showSignInform = (e) => {
+  const mainIntro = e.target.parentNode.parentNode;
+  if (form.style.display === 'none') {
+    form.style.display = 'block';
+    mainIntro.style.display = 'none';
+  } else {
+    form.style.display = 'none';
+    mainIntro.style.display = 'block';
+  }
+}
+/*
 		showSignUpform = (e) => {
 			const mainIntro = e.target.parentNode.parentNode;
 			if (form_main.style.display === 'none') {
